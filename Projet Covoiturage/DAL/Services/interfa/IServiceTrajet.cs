67e6bdbb251c01773e0ea5a3ed1ecbbd883ac1cb
mcduffben <mcduff.benjamin.cem@gmail.com>
2018-11-27 14:08:56 -0500

    trajet controleur view et service debut

---
+++ Projet Covoiturage/DAL/Services/interfa/IServiceTrajet.cs
@@ -0,0 +1,22 @@
+﻿using Projet_Covoiturage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Covoiturage.DAL.Services.interfa
+{
+        interface IServiceTrajet
+    {
+        List<Trajet> Filtre(string depar, string arriver);
+
+        void Reserver(ApplicationUser client, int trajetId);
+
+        void AnnuleReserver(ApplicationUser client, int trajetId);
+
+        void CreateTrajet(ApplicationUser chaufeur, Trajet courrent);
+
+        Trajet GetTrajet(int id);
+    }
+}
