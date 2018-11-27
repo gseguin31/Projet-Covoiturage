83fa4303f98fe13f99b0aaa0da20f7c688e609db
Allain Alexandre <1467772@cegepmontpetit.ca>
2018-11-27 12:16:06 -0500

    generic Repo

---
+++ Projet Covoiturage/DAL/Services/IServiceChauffeur.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet_Covoiturage.DAL.Services
+{
+    public class IServiceChauffeur
+    {
+    }
+}
