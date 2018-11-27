881c87a2c606f0d00cb18ea1c9f49fd4955fb9e1
Allain Alexandre <1467772@cegepmontpetit.ca>
2018-11-27 12:27:12 -0500

    test

--- Projet Covoiturage/DAL/Services/IServiceChauffeur.cs
+++ Projet Covoiturage/DAL/Services/IServiceChauffeur.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Projet_Covoiturage.DAL.Services
 {
-    public class IServiceChauffeur
+    interface IServiceChauffeur
     {
     }
 }
